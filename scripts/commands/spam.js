0f08db6da6cf4c3087b34577391e6cad29e248df
Riaz Mahmud Arman <armanhossin6000@gmail.com>
2024-09-18 15:55:43 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["riazmahudarman2038"];
+  const permission = ["100049478452177"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
